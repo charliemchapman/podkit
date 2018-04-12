be5b372fa2ed600d0f49038f9b6b4693d86c2b7b
Charlie Chapman <charliemchapman@gmail.com>
2018-04-11 21:09:01 -0500

    updated styles around description field

--- components/EpisodeEditor.js
+++ components/EpisodeEditor.js
@@ -22,23 +22,44 @@ class EpisodeEditor extends React.Component {
         return <LabelInput label={label} value={value} onChange={onChange}/>
     }
 
+    getDescriptionForm(){
+        const onChange = (e)=>{
+            const newValue = e.target.value;
+            const newEpisode = {...this.props.episodeXml}
+            newEpisode['description'] =  [newValue];
+            this.props.updateEpisode(newEpisode);
+        };
+        const value = this.props.episodeXml['description'] ? this.props.episodeXml['description'][0] : "";
+
+        return (
+            <div className="label-textarea">
+                <Typography variant="body1">Description</Typography>
+                <textarea value={value} onChange={onChange}/>
+            </div>
+        )
+    }
+
     getEpisodeForm() {
         return (
             <div className="feed-settings">
-                {this.getChannelItemForm('Title', 'title')}
-                {this.getChannelItemForm('dc:creator', 'dc:creator')}
-                {this.getChannelItemForm('pubDate', 'pubDate')}
-                {this.getChannelItemForm('link', 'link')}
-                {this.getChannelItemForm('guid', 'guid')}
-                {this.getChannelItemForm('description', 'description')}
-                {this.getChannelItemForm('itunes:author', 'itunes:author')}
-                {this.getChannelItemForm('itunes:subtitle', 'itunes:subtitle')}
-                {this.getChannelItemForm('itunes:explicit', 'itunes:explicit')}
-                {this.getChannelItemForm('itunes:duration', 'itunes:duration')}
-                <LabelInput label="itunes:image"/>
-                {this.getChannelItemForm('content:encoded', 'content:encoded')}
-                <LabelInput label="enclosure"/>
-                <LabelInput label="media:content"/>
+                <section className="simple-fields">
+                    {this.getChannelItemForm('Title', 'title')}
+                    {this.getChannelItemForm('dc:creator', 'dc:creator')}
+                    {this.getChannelItemForm('pubDate', 'pubDate')}
+                    {this.getChannelItemForm('link', 'link')}
+                    {this.getChannelItemForm('guid', 'guid')}
+                    {this.getChannelItemForm('itunes:author', 'itunes:author')}
+                    {this.getChannelItemForm('itunes:subtitle', 'itunes:subtitle')}
+                    {this.getChannelItemForm('itunes:explicit', 'itunes:explicit')}
+                    {this.getChannelItemForm('itunes:duration', 'itunes:duration')}
+                    <LabelInput label="itunes:image"/>
+                    {this.getChannelItemForm('content:encoded', 'content:encoded')}
+                    <LabelInput label="enclosure"/>
+                    <LabelInput label="media:content"/>
+                </section>
+                <section>
+                    { this.getDescriptionForm() }
+                </section>
             </div>
         );
     }
