d83b01bc17f19b0d80c735522bc52ec3911448a7
Charlie Chapman <charliemchapman@gmail.com>
2018-04-14 20:49:59 -0500

    using TextField for all inputs - moving toward material design

--- components/LabelInput.js
+++ components/LabelInput.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Typography from 'material-ui/Typography';
+import { TextField } from 'material-ui';
 
 export default ({label, value, onChange})=>{
     return (
-        <div>
-            <Typography variant="body1">{label}</Typography>
-            <input value={value} onChange={onChange}/>
+        <div className="label-input">
+            <TextField
+                label={label}
+                value={value}
+                onChange={onChange}
+                fullWidth
+                margin="normal"/>
         </div>
     )
 }
