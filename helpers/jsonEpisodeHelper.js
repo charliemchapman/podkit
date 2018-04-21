60cacb43ea8f0d89a82c43c6bbd879360b7975aa
mattjopete <matthewpeterein@gmail.com>
2018-04-20 21:54:52 -0500

    Fix episode guid and image parsing

--- helpers/jsonEpisodeHelper.js
+++ helpers/jsonEpisodeHelper.js
@@ -3,11 +3,11 @@ export const xmlEpisodeToJson = (xmlEpisode) => {
         title: xmlEpisode.title[0],
         content: xmlEpisode["content:encoded"] ? xmlEpisode["content:encoded"][0] : "",
         description: xmlEpisode.description[0],
-        guid: xmlEpisode.guid[0],
+        guid: getGuidFromXmlFeed(xmlEpisode.guid),
         author: xmlEpisode["itunes:author"] ? xmlEpisode["itunes:author"][0] : "",
         duration: xmlEpisode["itunes:duration"] ? xmlEpisode["itunes:duration"][0] : "",
         explicit: xmlEpisode["itunes:explicit"] ? xmlEpisode["itunes:explicit"][0] : "",
-        image: xmlEpisode["itunes:image"][0].$.href,
+        image: xmlEpisode["itunes:image"] ? xmlEpisode["itunes:image"][0].$.href : "",
         subtitle: xmlEpisode["itunes:subtitle"] ? xmlEpisode["itunes:subtitle"][0] : "",
         summary: xmlEpisode["itunes:summary"] ? xmlEpisode["itunes:summary"][0] : "",
         link: xmlEpisode.link[0],
@@ -46,4 +46,14 @@ export const episodeJsonToXmlFeed = (episodeJson) => {
         'enclosure': [ { $: {isDefault: "", length: "", medium: "", type: "", url: ""}}],
         'media:content':[ { $: {isDefault: "", length: "", medium: "", type: "", url: ""}}]
     }
+}
+
+function getGuidFromXmlFeed(xmlEpisodeGuid) {
+    var guid = xmlEpisodeGuid 
+        ? xmlEpisodeGuid[0]._
+            ? xmlEpisodeGuid[0]._
+            : xmlEpisodeGuid[0]
+        : "";
+
+    return guid.trim()
 }
